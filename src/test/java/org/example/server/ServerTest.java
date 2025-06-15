5fd68eda2b623e0b7ad4df3f323ec9151fffb997
vkyl <viktor.kyliukh@gmail.com>
2025-06-16 00:30:13 +0300

    Test for UDP

--- src/test/java/org/example/server/ServerTest.java
+++ src/test/java/org/example/server/ServerTest.java
@@ -34,17 +34,41 @@ class ServerTest {
     @Test
     @SneakyThrows
     void testUDP_THROWS_TIMEOUT() {
-        Server server = new Server(8080, InetAddress.getLocalHost(), ":PREFIX:", 1);
-        CommunicationUDP clientUdp = new CommunicationUDP(InetAddress.getLocalHost(), 8081, ":PREFIX:", 1);
+        Server server = new Server(8081, InetAddress.getLocalHost(), ":PREFIX:", 1);
+        CommunicationUDP clientUdp = new CommunicationUDP(InetAddress.getLocalHost(), 8082, ":PREFIX:", 1);
         new Thread(){
             @Override
             @SneakyThrows
             public void run() {
                 Thread.sleep(500);
                 assertThrows(SocketTimeoutException.class, () -> clientUdp.write("Hello!"));
+                clientUdp.close();
             }
         }.start();
+    }
 
-
+    @Test
+    @SneakyThrows
+    void testUDP() {
+        Server server = new Server(8083, InetAddress.getLocalHost(), ":PREFIX:", 1);
+        CommunicationUDP clientUdp = new CommunicationUDP(InetAddress.getLocalHost(), 8084, ":PREFIX:", 1);
+        new Thread(){
+            @Override
+            @SneakyThrows
+            public void run() {
+                ICommunication serverUdp = server.listen();
+                assertEquals("Hello!", serverUdp.read());
+                serverUdp.close();
+            }
+        }.start();
+        new Thread(){
+            @Override
+            @SneakyThrows
+            public void run() {
+                Thread.sleep(500);
+                clientUdp.write("Hello!");
+                clientUdp.close();
+            }
+        }.start();
     }
 }
